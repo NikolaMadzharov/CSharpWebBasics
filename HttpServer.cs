b025aa6d1cf9b423c98575bb736ef4c2eb1070e9
nikola madzharov <nmadzharov1912@gmail.com>
2022-08-23 23:44:34 +0300

    Added new extensions to the server

---
+++ HttpServer.cs
@@ -0,0 +1,99 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+
+
+public class HttpServer
+{
+    private readonly IPAddress ipAddress;
+    private readonly int port;
+    private readonly TcpListener serverListener;
+
+    private readonly RoutingTable routingTable;
+
+    public HttpServer(string ipAdress, int port,Action<IRoutingTable> routingTableConfiguration)
+    {
+
+        this.ipAddress = IPAddress.Parse(ipAdress);
+        this.port = port;
+
+        this.serverListener = new TcpListener(this.ipAddress, port);
+
+        routingTableConfiguration(this.routingTable = new RoutingTable());
+
+
+
+    }
+
+    public HttpServer(int port, Action<IRoutingTable> routingTable):this("127.0.0.1", port, routingTable)
+    {
+    }
+
+    public HttpServer(Action<IRoutingTable> routingTable):this(8080, routingTable)
+    {
+    }
+
+
+    public void Start()
+    {
+
+
+        this.serverListener.Start();
+
+        Console.WriteLine($"Server started on port {this.port}.");
+        Console.WriteLine("Listening for requests...");
+        while (true)
+        {
+            
+
+            var connection = serverListener.AcceptTcpClient();
+
+            var networkStream = connection.GetStream();
+
+            var requestText = this.ReadRequest(networkStream);
+
+            Console.WriteLine(requestText);
+
+            var request = Request.Parse(requestText);
+
+            var response = this.routingTable.MatchRequest(request);
+
+            WriteResponse(networkStream, response);
+
+            connection.Close();
+        }
+
+    }
+
+    private void WriteResponse(NetworkStream networkStream, Response response)
+    {
+        var responseBytes = Encoding.UTF8.GetBytes(response.ToString());
+
+        networkStream.Write(responseBytes);
+
+    }
+
+    private string ReadRequest(NetworkStream networkStream)
+    {
+
+        var bufferLength = 1024;
+        var buffer = new byte[bufferLength];
+
+        var requestBuilder = new StringBuilder();
+
+        do
+        {
+            var bytesRead = networkStream.Read(buffer, 0, bufferLength);
+
+            requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+
+        }
+        while (networkStream.DataAvailable);
+
+        return requestBuilder.ToString();
+
+
+    }
+}
