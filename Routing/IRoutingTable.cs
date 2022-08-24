75faf96a001fe8f42dcce08a4fb8fef9224310b0
Nikola Madzharov <89745007+koko1912@users.noreply.github.com>
2022-08-24 23:33:33 +0300

    Delete Routing directory

--- Routing/IRoutingTable.cs
+++
@@ -1,8 +0,0 @@
-﻿public interface IRoutingTable
-{
-    IRoutingTable Map(string url, Method method, Response response);
-
-    IRoutingTable MapGet(string url, Response response);
-
-    IRoutingTable MapPost(string url, Response response);
-}
