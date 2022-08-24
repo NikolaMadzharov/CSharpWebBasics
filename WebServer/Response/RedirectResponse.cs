5f672ee556684340143bec22cb65c02a2c489764
nikola madzharov <nmadzharov1912@gmail.com>
2022-08-24 23:36:21 +0300

    Added Sync to the server and download files

---
+++ WebServer/Response/RedirectResponse.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+
+public class RedirectResponse:Response
+{
+    public RedirectResponse(string location)
+        : base(StatusCode.Found)
+    {
+        this.Headers.Add(Header.Location,location);
+    }
+}
